65d85c9dd74edea6f8926b6a951d05dc33efb63e
varenaya <varenayanegi@gmail.com>
2021-09-13 23:40:33 +0530

    This is First Commit

---
+++ android/app/src/main/kotlin/com/example/fichat/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fichat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,393 @@
+import 'dart:convert';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_webrtc/flutter_webrtc.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FiChat',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Fichat'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+typedef void StreamStateCallback(MediaStream stream);
+
+class _MyHomePageState extends State<MyHomePage> {
+  final _localrender = new RTCVideoRenderer();
+  final _remoterender = new RTCVideoRenderer();
+
+  Map<String, dynamic> configuration = {
+    'iceServers': [
+      {
+        'urls': [
+          'stun:stun1.l.google.com:19302',
+          'stun:stun2.l.google.com:19302'
+        ]
+      }
+    ]
+  };
+
+  RTCPeerConnection? peerConnection;
+  MediaStream? localStream;
+  MediaStream? remoteStream;
+  String? roomId;
+  String? currentRoomText;
+  StreamStateCallback? onAddRemoteStream;
+
+  TextEditingController textEditingController = TextEditingController(text: '');
+
+  @override
+  void initState() {
+    initrenderer();
+    onAddRemoteStream = ((stream) {
+      _remoterender.srcObject = stream;
+      setState(() {});
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _localrender.dispose();
+    _remoterender.dispose();
+    super.dispose();
+  }
+
+  initrenderer() async {
+    await _localrender.initialize();
+    await _remoterender.initialize();
+  }
+
+  _getusermedia() async {
+    final mediaconfig = {
+      'audio': true,
+      'video': {
+        'facingmode': 'user',
+      }
+    };
+
+    MediaStream stream = await navigator.mediaDevices.getUserMedia(mediaconfig);
+    _localrender.srcObject = stream;
+    localStream = stream;
+
+    _remoterender.srcObject = await createLocalMediaStream('key');
+  }
+
+  void registerPeerConnectionListeners() {
+    peerConnection?.onIceGatheringState = (RTCIceGatheringState state) {
+      print('ICE gathering state changed: $state');
+    };
+
+    peerConnection?.onConnectionState = (RTCPeerConnectionState state) {
+      print('Connection state change: $state');
+    };
+
+    peerConnection?.onSignalingState = (RTCSignalingState state) {
+      print('Signaling state change: $state');
+    };
+
+    peerConnection?.onIceGatheringState = (RTCIceGatheringState state) {
+      print('ICE connection state change: $state');
+    };
+
+    peerConnection?.onAddStream = (MediaStream stream) {
+      print("Add remote stream");
+      onAddRemoteStream?.call(stream);
+      remoteStream = stream;
+    };
+  }
+
+  createRoom(remoteRenderer) async {
+    FirebaseFirestore db = FirebaseFirestore.instance;
+    DocumentReference roomRef = db.collection('rooms').doc();
+
+    print('Create PeerConnection with configuration: $configuration');
+
+    peerConnection = await createPeerConnection(configuration);
+
+    registerPeerConnectionListeners();
+
+    localStream?.getTracks().forEach((track) {
+      peerConnection?.addTrack(track, localStream!);
+    });
+
+    // Code for collecting ICE candidates below
+    var callerCandidatesCollection = roomRef.collection('callerCandidates');
+
+    peerConnection?.onIceCandidate = (RTCIceCandidate candidate) {
+      print('Got candidate: ${candidate.toMap()}');
+      callerCandidatesCollection.add(candidate.toMap());
+    };
+    // Finish Code for collecting ICE candidate
+
+    // Add code for creating a room
+    RTCSessionDescription offer = await peerConnection!.createOffer();
+    await peerConnection!.setLocalDescription(offer);
+    print('Created offer: $offer');
+
+    Map<String, dynamic> roomWithOffer = {'offer': offer.toMap()};
+
+    await roomRef.set(roomWithOffer);
+    var roomId = roomRef.id;
+    print('New room created with SDK offer. Room ID: $roomId');
+    currentRoomText = 'Current room is $roomId - You are the caller!';
+    // Created a Room
+
+    peerConnection?.onTrack = (RTCTrackEvent event) {
+      print('Got remote track: ${event.streams[0]}');
+
+      event.streams[0].getTracks().forEach((track) {
+        print('Add a track to the remoteStream $track');
+        remoteStream?.addTrack(track);
+      });
+    };
+
+    // Listening for remote session description below
+    roomRef.snapshots().listen((snapshot) async {
+      print('Got updated room: ${snapshot.data()}');
+
+      Map<String, dynamic> data = snapshot.data() as Map<String, dynamic>;
+      if (peerConnection?.getRemoteDescription() != null &&
+          data['answer'] != null) {
+        var answer = RTCSessionDescription(
+          data['answer']['sdp'],
+          data['answer']['type'],
+        );
+
+        print("Someone tried to connect");
+        await peerConnection?.setRemoteDescription(answer);
+      }
+    });
+    // Listening for remote session description above
+
+    // Listen for remote Ice candidates below
+    roomRef.collection('calleeCandidates').snapshots().listen((snapshot) {
+      snapshot.docChanges.forEach((change) {
+        if (change.type == DocumentChangeType.added) {
+          Map<String, dynamic> data = change.doc.data() as Map<String, dynamic>;
+          print('Got new remote ICE candidate: ${jsonEncode(data)}');
+          peerConnection!.addCandidate(
+            RTCIceCandidate(
+              data['candidate'],
+              data['sdpMid'],
+              data['sdpMLineIndex'],
+            ),
+          );
+        }
+      });
+    });
+    // Listen for remote ICE candidates above
+
+    return roomId;
+  }
+
+  void joinRoom(String text, RTCVideoRenderer remoterender) async {
+    FirebaseFirestore db = FirebaseFirestore.instance;
+    DocumentReference roomRef = db.collection('rooms').doc('$roomId');
+    var roomSnapshot = await roomRef.get();
+    print('Got room ${roomSnapshot.exists}');
+
+    if (roomSnapshot.exists) {
+      print('Create PeerConnection with configuration: $configuration');
+      peerConnection = await createPeerConnection(configuration);
+
+      registerPeerConnectionListeners();
+
+      localStream?.getTracks().forEach((track) {
+        peerConnection?.addTrack(track, localStream!);
+      });
+
+      // Code for collecting ICE candidates below
+      var calleeCandidatesCollection = roomRef.collection('calleeCandidates');
+      peerConnection!.onIceCandidate = (RTCIceCandidate candidate) {
+        if (candidate == null) {
+          print('onIceCandidate: complete!');
+          return;
+        }
+        print('onIceCandidate: ${candidate.toMap()}');
+        calleeCandidatesCollection.add(candidate.toMap());
+      };
+      // Code for collecting ICE candidate above
+
+      peerConnection?.onTrack = (RTCTrackEvent event) {
+        print('Got remote track: ${event.streams[0]}');
+        event.streams[0].getTracks().forEach((track) {
+          print('Add a track to the remoteStream: $track');
+          remoteStream?.addTrack(track);
+        });
+      };
+
+      // Code for creating SDP answer below
+      var data = roomSnapshot.data() as Map<String, dynamic>;
+      print('Got offer $data');
+      var offer = data['offer'];
+      await peerConnection?.setRemoteDescription(
+        RTCSessionDescription(offer['sdp'], offer['type']),
+      );
+      var answer = await peerConnection!.createAnswer();
+      print('Created Answer $answer');
+
+      await peerConnection!.setLocalDescription(answer);
+
+      Map<String, dynamic> roomWithAnswer = {
+        'answer': {'type': answer.type, 'sdp': answer.sdp}
+      };
+
+      await roomRef.update(roomWithAnswer);
+      // Finished creating SDP answer
+
+      // Listening for remote ICE candidates below
+      roomRef.collection('callerCandidates').snapshots().listen((snapshot) {
+        snapshot.docChanges.forEach((document) {
+          var data = document.doc.data() as Map<String, dynamic>;
+          print(data);
+          print('Got new remote ICE candidate: $data');
+          peerConnection!.addCandidate(
+            RTCIceCandidate(
+              data['candidate'],
+              data['sdpMid'],
+              data['sdpMLineIndex'],
+            ),
+          );
+        });
+      });
+    }
+  }
+
+  void hangUp(localRenderer) async {
+    List<MediaStreamTrack> tracks = localRenderer.srcObject!.getTracks();
+    tracks.forEach((track) {
+      track.stop();
+    });
+
+    if (remoteStream != null) {
+      remoteStream!.getTracks().forEach((track) => track.stop());
+    }
+    if (peerConnection != null) peerConnection!.close();
+
+    if (roomId != null) {
+      var db = FirebaseFirestore.instance;
+      var roomRef = db.collection('rooms').doc(roomId);
+      var calleeCandidates = await roomRef.collection('calleeCandidates').get();
+      calleeCandidates.docs.forEach((document) => document.reference.delete());
+
+      var callerCandidates = await roomRef.collection('callerCandidates').get();
+      callerCandidates.docs.forEach((document) => document.reference.delete());
+
+      await roomRef.delete();
+    }
+
+    localStream!.dispose();
+    remoteStream?.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Column(
+        children: [
+          SizedBox(height: 8),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  _getusermedia();
+                },
+                child: Text("Open camera & microphone"),
+              ),
+              SizedBox(
+                width: 8,
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  roomId = await createRoom(_remoterender);
+                  textEditingController.text = roomId!;
+                  setState(() {});
+                },
+                child: Text("Create room"),
+              ),
+              SizedBox(
+                width: 8,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  // Add roomId
+                  joinRoom(
+                    textEditingController.text,
+                    _remoterender,
+                  );
+                },
+                child: Text("Join room"),
+              ),
+              SizedBox(
+                width: 8,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  hangUp(_localrender);
+                },
+                child: Text("Hangup"),
+              )
+            ],
+          ),
+          SizedBox(height: 8),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(child: RTCVideoView(_localrender, mirror: true)),
+                  Expanded(child: RTCVideoView(_remoterender)),
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Join the following Room: "),
+                Flexible(
+                  child: TextFormField(
+                    controller: textEditingController,
+                  ),
+                )
+              ],
+            ),
+          ),
+          SizedBox(height: 8)
+        ],
+      ),
+    );
+  }
+}
